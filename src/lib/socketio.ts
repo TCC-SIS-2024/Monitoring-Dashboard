c1eee0bae3836c1cddf12717f7baede5f256e55b
lucas emanoel <lucws2002@gmail.com>
2024-11-25 16:17:38 -0400

    fix: host to connect to api and socket

--- src/lib/socketio.ts
+++ src/lib/socketio.ts
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
 
-export const socketIoClient = io('http://localhost:8000/', {
+export const socketIoClient = io('http://0.0.0.0:8000/', {
   transports: ["websocket"]
 })
